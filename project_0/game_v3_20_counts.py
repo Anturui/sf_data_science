1628b8120e72aaf65b3f74529e6a0a1d20c569f7
Anturui <arwielaoanturui@gmail.com>
2022-06-01 15:38:50 +0300

    edit 20 counts

--- project_0/game_v3_20_counts.py
+++ project_0/game_v3_20_counts.py
@@ -20,6 +20,8 @@ def random_predict(number:int=1) -> int:
         predict_number = np.random.randint(1, 101) # предполагаемое число
         if number == predict_number:
             break # выход из цикла, если угадали
+        if number > predict_number:
+            
     return(count)
 print(f'Количество попыток: {random_predict()}')
 
